3aa2c1960a8ba93c04453fa2a02850d6ca774ef7
tiankaiqiang <374725192@qq.com>
2020-12-11 17:28:42 +0800

    hhhh

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -81,6 +81,7 @@ export default {
           userName,
           password
         }).then(res => {
+         
           const data = res.data
           commit('setToken', data.token)
           resolve()
